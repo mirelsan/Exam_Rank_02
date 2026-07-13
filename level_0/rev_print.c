0618b196e923341c68c4c6e51eaa37b93474875d
Mirela Santos Oliveira <mirelsan42student@gmail.com>
2026-07-13 01:29:42 +0000

    add level 0 solutions

--- level_0/rev_print.c
+++ level_0/rev_print.c
@@ -3,7 +3,11 @@
 int ft_strlen(char *str)
 {
     int i = 0;
-    while
+    while(str[i])
+    {
+        i++;
+    }
+    return(i);
 }
 
 
@@ -15,10 +19,14 @@ int main(int argc, char **argv)
         return(0);
     }
 
-    int i = 0;
+    int i;
 
-    while(argv[1][i])
+    i = ft_strlen(argv[1]) - 1;
+    while(i >= 0)
     {
-
+        write(1, &argv[1][i], 1);
+        --i;
     }
+    write(1, "\n", 1);
+    return(0);
 }
